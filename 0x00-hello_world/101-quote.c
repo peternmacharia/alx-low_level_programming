1216081848a4e212660b14cd2992bb00351fd1e6
peternmacharia <peternmacharia@outlook.com>
2022-06-30 23:29:55 +0300

    UNIX commit #18

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
+#include <unistd.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(2, str1, 59);
 	return (1);
 }
