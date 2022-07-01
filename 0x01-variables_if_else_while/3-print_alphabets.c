a5ab42bb187109c54d893c537c0205729987b4db
peternmacharia <peternmacharia@outlook.com>
2022-07-01 22:58:58 +0300

    Alphabet commit #4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int alphABET;
 
-	alphABET='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n';
+	alphABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
 	putchar(alphABET);
 	return (0);
 }
