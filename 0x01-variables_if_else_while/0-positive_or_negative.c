1a81e8bef5ad948ac026024937fc6826761454c2
peternmacharia <peternmacharia@outlook.com>
2022-07-01 16:14:04 +0300

    Positive anything commit #7

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -13,13 +13,16 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0) {
+if (n > 0) 
+{
 	printf("%d is positive\n", n);
 }
-if (n = 0) {
+if (n = 0) 
+{
 	printf("%d is zero\n", n);
 }
-if (n < 0) {
+if (n < 0) 
+{
 	printf("%d is negative\n", n);
 }
 return (0);
