6819f438d2ade7e2e53827353f9d0656e3e20901
peternmacharia <peternmacharia@outlook.com>
2022-07-01 16:12:01 +0300

    Positive anything commit #6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
 	* 
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,13 +13,14 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if ( n > 0 ) {
+if (n > 0) {
 	printf("%d is positive\n", n);
 }
-else if ( n = 0 ) {
+if (n = 0) {
 	printf("%d is zero\n", n);
 }
-else if ( n < 0 ) {
+if (n < 0) {
 	printf("%d is negative\n", n);
 }
+return (0);
 }
