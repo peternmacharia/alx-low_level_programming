c6239f02dd015d175bdda46eb7e6fd61020174f9
peternmacharia <peternmacharia@outlook.com>
2022-07-01 22:36:52 +0300

    I sometimes suffer from insomnia commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+	* Return: Always 1 (Success)
+ */
+int main(void)
+{
+char alphas;
+
+alphas="abcdefghijklmnopqrstuvwxyz";
+putchar(alphas);
+return (0);
+}
