684eab4a169c8a71717b3e6773908ab75c90e785
peternmacharia <peternmacharia@outlook.com>
2022-07-01 22:55:23 +0300

    Alphabet commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+	* Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n';
+	putchar(alphABET);
+	return (0);
+}
