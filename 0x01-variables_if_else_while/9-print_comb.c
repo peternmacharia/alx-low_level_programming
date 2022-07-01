31362b6251d8118b0dbd2fa11dabfdaa0c7ebe44
peternmacharia <peternmacharia@outlook.com>
2022-07-02 01:58:20 +0300

    Patience persistence and perspiration commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints numbers between 0 to 9 with commas and spaces.
  *
-	* Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 48; i < 58; i++)
-{
-	putchar(i);
-	if (i != 57)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
