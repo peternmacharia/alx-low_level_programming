45ecf865c58bac00237a74ed9a1d5d7c2bf8630c
peternmacharia <peternmacharia@outlook.com>
2022-07-01 16:03:05 +0300

    Positive anything commit #2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ if (n > 0) {
 	printf("%d is positive\n", n);
 }
 else if (n = 0) {
-	printf("%d is zero\n");
+	printf("%d is zero\n", n);
 }
 else if (n < 0) {
-	printf("%d is negative\n");
+	printf("%d is negative\n", n);
 }
 }
