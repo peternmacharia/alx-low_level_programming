e3aee0a502a1b18abcc4ecc9c69a1a1512c36506
peternmacharia <peternmacharia@outlook.com>
2022-07-01 23:38:02 +0300

    Numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+	* Return: Always 1 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	printf("%d\n", i);
+}
+return (0);
+}
