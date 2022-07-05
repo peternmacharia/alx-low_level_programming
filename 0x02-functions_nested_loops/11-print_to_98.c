1a7463d25d23ec2f7bad163c8facc30d032ccbaf
peternmacharia <peternmacharia@outlook.com>
2022-07-05 23:07:31 +0300

    Battery Street commit #4

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,19 @@
  */
 void print_to_98(int n)
 {
-n = 0;
-while (n <= 98)
+if(n < 98)
 {
-	_putchar(n);
-	_putchar(',');
-	_putchar(' ');
-	n++;
+	for (; n < 98; n++)
+	{
+		printf("%d, ", n);
+	}
 }
+else if (n > 98)
+{
+	for (; n > 98; n--)
+	{
+		printf("%d, ", n);
+	}
+}
+printf("%d\n", n);
 }
