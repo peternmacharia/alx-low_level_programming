4d8cc58f1b27a1677e51051bda9c987c4816dff5
peternmacharia <peternmacharia@outlook.com>
2022-07-05 16:26:38 +0300

    Sign commit #2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 int print_sign(int n)
 {
-if (n <= 0)
+if (n < 0)
 {
 	printf("-");
 	return (-1);
@@ -17,7 +17,7 @@ else if (n == 0)
 	printf(0);
 	return (0);
 }
-else if (n >= 0)
+else if (n > 0)
 {
 	printf("+");
 	return (1);
