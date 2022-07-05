1b561de537eda05187de4acd4edbfb9ac83391bd
peternmacharia <peternmacharia@outlook.com>
2022-07-05 23:40:53 +0300

    Times table commit #4

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,14 +11,14 @@ void print_times_table(int n)
 {
 int i, j, mult;
 
-if (n > 0 && n < 15)
+if (n >= 0 && n <= 15)
 {
-	for (i = 1; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		printf("0, ");
 		for (j = 1; j <= n; j++)
 		{
-			mult = n * n;
+			mult = i * j;
 			printf("%d, ", mult);
 		}
 	}
