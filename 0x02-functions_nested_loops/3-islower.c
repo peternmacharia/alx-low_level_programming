72f8f6ee4d655cc4eceef749cec0663356d10027
peternmacharia <peternmacharia@outlook.com>
2022-07-05 18:01:00 +0300

    islower commit #3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
  * _islower - Checks the lowercase character
+	*
+	*@c: is an integer
  *
 	* Return: return 1 (if lowercase) return 0 (otherwise)
  */
@@ -15,4 +17,5 @@ else
 {
 	return (0);
 }
+_putchar('\n');
 }
