785d77a1fd34759b5d08bef199d5973b133ab3aa
peternmacharia <peternmacharia@outlook.com>
2022-07-05 19:00:37 +0300

    Jack Bauer commit #7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,17 +14,8 @@ for (i = 48; i <= 50; i++)
 {
 	for (j = 48; j <= 51; j++)
 	{
-		for (m = 48; m <= 53; m++)
-		{
-			for (n = 48; n <= 57; n++)
-			{
-				_putchar(i);
-				_putchar(j);
-				_putchar(':');
-				_putchar(m);
-				_putchar(n);
-			}
-		}
+		_putchar(i);
+		_putchar(j);
 	}
 }
 _putchar('\n');
