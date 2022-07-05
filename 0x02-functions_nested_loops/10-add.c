68d4503d390b5b827daea68ef7e488e3bf2fc6ab
peternmacharia <peternmacharia@outlook.com>
2022-07-05 22:41:05 +0300

    Add two numbers commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers
+	*
+	* @i, @j: this are integers
+ * 
+	* Return: Returns the result
+ */
+int add(int i, int j)
+{
+int result;
+
+result = i + j;
+_putchar(result);
+return (result);
+}
