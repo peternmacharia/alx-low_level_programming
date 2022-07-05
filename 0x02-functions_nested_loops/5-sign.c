8a82cc7ea0b5b46f4a3bcfe5fe6e8d6b846fdc5e
peternmacharia <peternmacharia@outlook.com>
2022-07-05 16:25:02 +0300

    Sign commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+	* Return: 1 (prints +), 0 (prints 0) and -1 (prints -)
+ */
+int print_sign(int n)
+{
+if (n <= 0)
+{
+	printf("-");
+	return (-1);
+}
+else if (n == 0)
+{
+	printf(0);
+	return (0);
+}
+else if (n >= 0)
+{
+	printf("+");
+	return (1);
+}
+}
