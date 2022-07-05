3e2caddc719d42a6cdbbf54c787687ca11b0acc5
peternmacharia <peternmacharia@outlook.com>
2022-07-05 14:45:04 +0300

    Insomnia commit #8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
-void print_alphabet(void);
-
+#include "main.h"
 /**
  * main - Entry point prints alphabets
  *
 	* Return: Always 0 (Success)
  */
-int main(void)
-{
-print_alphabet();
-return 0;
-}
-
 void print_alphabet(void)
 {
 int i;
 
 for (i = 93; i < 123; i++)
 {
-	printf("%d", i);
+	_putchar(i);
 }
+_putchar('\n');
 }
