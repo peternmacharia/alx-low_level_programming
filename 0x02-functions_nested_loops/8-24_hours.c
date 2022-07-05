00663a1eeba52998b874eb3a5224dc2eece3153f
peternmacharia <peternmacharia@outlook.com>
2022-07-05 19:10:17 +0300

    Jack Bauer commit #10

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,24 @@
  */
 void jack_bauer(void)
 {
-int i, j;
+int i, j, m, n;
 
 for (i = 48; i <= 50; i++)
 {
-	for (j = 48; j <= 51; j++)
+	for (j = 48; j <= 57; j++)
 	{
-		_putchar(i);
-		_putchar(j);
-		_putchar(':');
+		for (m = 48; m <= 53; m++)
+		{
+			for (n = 48; n <= 57; n++)
+			{
+				_putchar(i);
+				_putchar(j);
+				_putchar(':');
+				_putchar(m);
+				_putchar(n);
+				_putchar('\n');
+			}
+		}
 	}
 }
-_putchar('\n');
 }
