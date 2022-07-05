f38033664ca44034e37677aecbfb8eddc6be06b4
peternmacharia <peternmacharia@outlook.com>
2022-07-05 10:17:43 +0300

    10x alphabet commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point prints 10 times alphabets
+ *
+	* Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 93; j < 123; j++)
+	{
+		putchar(j);
+	}
+}
+return (0);
+}
