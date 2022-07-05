fc57c20150182e47bbcfc822e5ab593056f44310
peternmacharia <peternmacharia@outlook.com>
2022-07-05 09:32:17 +0300

    Sometimes I suffer from Insomnia commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point prints alphabets
+ *
+	* Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 93; i < 123; i++)
+{
+	printf("%d\n", i);
+}
+return (0);
+}
