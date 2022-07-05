b4d56e5a4dcca903b050d6018e28194dfadee043
peternmacharia <peternmacharia@outlook.com>
2022-07-05 23:27:49 +0300

    Times table commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - Function that prints times table
+	*
+	*@n: this are integers
+	*
+ * Return: Times table
+ */
+void print_times_table(int n)
+{
+int i, j, mult;
+
+if (n > 0 && n < 15)
+{
+	for (i = n; n <= n; n++)
+	{
+		printf("0, ");
+		for (j = n; j <= n; j++)
+		{
+			mult = n * n;
+			printf("%d, ", n);
+		}
+	}
+}
+}
