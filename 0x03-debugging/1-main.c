10a9a979e936ce5c42a80e07cd460abd6e5ae7ed
peternmacharia <peternmacharia@outlook.com>
2022-07-06 09:57:14 +0300

    Like comment subscribe commit #6

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 int i;
@@ -18,7 +17,7 @@ i = 0;
 	putchar(i);
 } */
 
-/* printf("Infinite loop avoided! \\o/\n"); */
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
