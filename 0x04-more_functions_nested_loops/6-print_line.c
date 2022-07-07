3014088bc80cfa6b287cf6b5c201686fb4b717cf
peternmacharia <peternmacharia@outlook.com>
2022-07-07 20:06:00 +0300

    Straight line commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a line in the terminal
+	*
+	* @n: Integer number
+	* @i: Inter number
+ *
+	* Return: Line in the terminal
+ */
+
+void print_line(int n)
+{
+int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
