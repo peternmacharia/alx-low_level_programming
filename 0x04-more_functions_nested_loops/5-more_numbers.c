292a7a3d3ced506e16b3d6f5bfb4f93f66060f99
peternmacharia <peternmacharia@outlook.com>
2022-07-07 19:41:08 +0300

    Universal language commit #2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - Prints 10 times the numbers from 0 to 14
 	*
- * Return: 10 times the numbers from 0 to 14 
+ * Return: 10 times the numbers from 0 to 14
  */
 void more_numbers(void)
 {
