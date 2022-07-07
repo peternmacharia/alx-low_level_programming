121d850e7f2f46079e9d9f9714b21a5907fcfc86
peternmacharia <peternmacharia@outlook.com>
2022-07-07 20:46:43 +0300

    Straight line commit #5

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,15 +12,15 @@ void print_line(int n)
 {
 int i;
 
-	if (n <= 0)
+if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar(95);
+		_putchar(95);
 		}
 	}
 }
