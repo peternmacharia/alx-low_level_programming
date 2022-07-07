3dd001a9d0b4539d73b765767089950ba4fa1fa2
peternmacharia <peternmacharia@outlook.com>
2022-07-07 21:11:47 +0300

    Number and signs commit #6

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,12 @@ void print_most_numbers(void)
 {
 	int m;
 
-for (m = 48; m <= 57; m++)
+	for (m = 48; m <= 57; m++)
 	{
 		if (m != 50 && m != 52)
 		{
 			_putchar(m);
 		}
+		_putchar('\n');
 	}
 }
