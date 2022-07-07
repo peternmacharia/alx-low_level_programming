07fa105285513853c758ba5503d6ae0f3044eb6a
peternmacharia <peternmacharia@outlook.com>
2022-07-07 18:54:11 +0300

    The number speak for themselves commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+	*
+ * Return: Numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+	_putchar(a);
+}
+_putchar('\n');
+}
