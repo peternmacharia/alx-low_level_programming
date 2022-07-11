b4325cc86aa17aae08866b961621c20a07759533
peternmacharia <peternmacharia@outlook.com>
2022-07-11 13:25:22 +0300

    Length os a string commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+	* @s: this is a string character
+ *
+	* Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(*s);
+
+	return (i);
+}
