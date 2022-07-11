78176646ade3868f26a8bb00584081921b6b448b
peternmacharia <peternmacharia@outlook.com>
2022-07-11 14:01:57 +0300

    Reverse gear commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+	* @s: a string character
+ *
+	* Return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
