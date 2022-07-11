875d3c8783834c4490cbff37215f0c0c9af4dbbc
peternmacharia <peternmacharia@outlook.com>
2022-07-11 12:52:04 +0300

    Battery commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter 
+	*
+	* @*n: an interger pointer
+ * Return: nothing to return
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+	*p = 98;
+	_putchar(n);
+}
