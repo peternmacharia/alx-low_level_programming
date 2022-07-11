51ec863c36e5b6e47054ca74dd558441f0357488
peternmacharia <peternmacharia@outlook.com>
2022-07-11 13:10:55 +0300

    Swap commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the valuew of two integers
+ * @a: first integer
+	* @b: second integer
+	*
+	* Return: nothing to return
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	a = &*c;
+	b = &*a;
+	c = &*b;
+}
