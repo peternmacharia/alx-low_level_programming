2d2a35d431ceb4902611ab818101034d7b1d63a3
peternmacharia <peternmacharia@outlook.com>
2022-07-11 14:14:40 +0300

    Reverse string commit #2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,24 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int count = 0, i, j;
+	char *str, temp;
 
-	while (i >= 0)
+	while (count >= 0)
 	{
-		if (s[i] == '\0')
-		{
+		if (s[count] == '\0')
 			break;
-		}
-		i++;
+		count++;
 	}
+	str = s;
 
-	for (i--; i >= 0; i--)
+	for (i = 0; i < (count - 1); i++)
 	{
-		_putchar(s[i]);
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
 }
