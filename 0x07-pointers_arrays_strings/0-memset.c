9ae110b396fcbd178396e849f53788f2381dd4fb
peternmacharia <peternmacharia@outlook.com>
2022-07-18 08:08:00 +0300

    More pointers arrays and strings file addition

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+	* @s: character variable
+	* @b: character variable
+	* @n: an unsigned integer variable
+ *
+	* Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
