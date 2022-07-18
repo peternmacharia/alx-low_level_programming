a9289852b15365707c89f61025d4be07d6b6c1ee
peternmacharia <peternmacharia@outlook.com>
2022-07-18 22:50:52 +0300

    Memset commit #5

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int j;
+	unsigned int i;
 
-for (j = 0; j < n; j++)
-{
-	*(s + j) = b;
-}
-return (s);
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
 }
