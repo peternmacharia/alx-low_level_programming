716fd5db1e30e23a4e38c1005cbcc437faebbedf
peternmacharia <peternmacharia@outlook.com>
2022-07-18 23:27:27 +0300

    Crack me commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,1 @@
-#include "main.h"
-
-/**
- * set_string - function that sets the value of a pointer to a char
-	* @s: character variable
-	* @to: variable character
-	*
- * Return: return nothing
- */
-
-void set_string(char **s, char *to)
-{
-	*s = to;
-}
+abc123
