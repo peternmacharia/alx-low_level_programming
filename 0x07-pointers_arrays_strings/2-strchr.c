b0b9eacadc818227658c0882f0dda574c8ca1757
peternmacharia <peternmacharia@outlook.com>
2022-07-18 23:01:29 +0300

    Strchr commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,5 +11,18 @@
 
 char *_strchr(char *s, char c)
 {
+	unsigned int j = 0;
 
+	for (; *(s + j) != '\0'; j++)
+	{
+		if (*(s + j) == c)
+		{
+			return (s + j);
+		}
+	}
+	if (*(s + j) == c)
+	{
+		return (s + j);
+	}
+	return ('\0');
 }
