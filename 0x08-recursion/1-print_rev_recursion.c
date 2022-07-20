ccd465c1d2daf0c91bd48179c594ac6a5756e10b
peternmacharia <peternmacharia@outlook.com>
2022-07-20 12:36:47 +0300

    My dreams commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+	* @s: character variable
+	*
+ * Return: reverse string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
