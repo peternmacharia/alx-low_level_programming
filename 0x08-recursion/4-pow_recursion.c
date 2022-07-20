c15f71abd95f2b167dd3c6f85ef0d41e8ba3211b
peternmacharia <peternmacharia@outlook.com>
2022-07-20 13:25:54 +0300

    Impossible to eradicate commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x
+	* raised to power of y
+	* @x: integer variable
+	* @y: integer variable
+	*
+ * Return: value of x raised to power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		return (-1);
+	}
+	else
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
