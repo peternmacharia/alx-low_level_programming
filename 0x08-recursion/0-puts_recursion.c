36e93cb8a085f4f91386902bfc02a1430abe0d6a
peternmacharia <peternmacharia@outlook.com>
2022-07-20 12:29:23 +0300

    She looked away a secret commit #10

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while ( *(s + i) != '\0')
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
-		i++;
+		_puts_recursion(s + 1);
 	}
-
-	_putchar('\n');
 }
