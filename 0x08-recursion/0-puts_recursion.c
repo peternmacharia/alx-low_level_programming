a73ae22a68bc36ff5040799ba16f1aa449834d06
peternmacharia <peternmacharia@outlook.com>
2022-07-20 12:08:02 +0300

    She looked away a secret commit #6

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string 
+ * _puts_recursion - function that prints a string
 	* followed by a new line
 	* @s: character variable
 	*
@@ -10,8 +10,8 @@
 
 void _puts_recursion(char *s)
 {
-	char i = &*s;
+	char i[] = &*s;
 
-	_putchar(i);
+	_putchar(i[]);
 	_putchar('\n');
 }
