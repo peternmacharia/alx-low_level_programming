0e38742074a567d4a5f1f81984d471465123d8a8
peternmacharia <peternmacharia@outlook.com>
2022-07-20 13:01:56 +0300

    Actually strange commit #4

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		i++;
-		_putchar(i);
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
 	}
 }
