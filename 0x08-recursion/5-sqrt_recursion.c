27cf96a029dbbe770cadf08451578f89a4686d17
peternmacharia <peternmacharia@outlook.com>
2022-07-20 14:58:48 +0300

    Inception commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+	* @n: integer variable
+	*
+ * Return: natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns 1 if the input is a prime number
+	* otherwise return 0
+	* @n: integer variable
+	*
+ * Return: returns 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if ((n / n) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
