3ffe7f57414fb181c6b6c7f5025f8790c1054c75
peternmacharia <peternmacharia@outlook.com>
2022-07-25 09:54:29 +0300

    What is your answer to commit #2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,19 +8,8 @@
 	*	Return: return 0 if Successful
  */
 
-int main(int argc, char* argv[])
+int main(int argc __attribute__((unused)), char* argv[])
 {
-	int i;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%d", argc);
-			printf("%s", argv[i]);
-		}
-		printf("\n");
-	}
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
