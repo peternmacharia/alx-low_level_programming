e964e134c18b8393427bb5c02d635370009740d1
peternmacharia <peternmacharia@outlook.com>
2022-07-25 10:00:15 +0300

    Silence is argument commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: integer variable
+	* @argv: pointer character variable
+	*
+	*	Return: return 0 if Successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
