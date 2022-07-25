4200034ba09a0d5ff70a7a1475d2b258dc555f55
peternmacharia <peternmacharia@outlook.com>
2022-07-25 06:19:02 +0300

    Library commit

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+	*
+ * @i: is an integer absolute value
+	*
+	* Return: Absolute value
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+	return (i);
+}
+else
+{
+	return (i * -1);
+}
+_putchar('\n');
+}
