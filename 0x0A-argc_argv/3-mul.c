0f83716afa5c36c7794ebb4e13fbdac1eb94a8d1
peternmacharia <peternmacharia@outlook.com>
2022-07-25 10:24:06 +0300

    Sarcasm is argument commit #2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,23 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int i, mul;
+	int mul;
 
-	if (argc == 0)
+	if (argc != 3)
 	{
-		return (-1);
-	}
-	if (argc <= 1)
-	{
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		for (i = 1; i < argc; i++)
-		{
-			mul *= atoi(argv[i]);
-			printf("%d\n", mul);
-		}
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 		return (0);
 	}
 }
