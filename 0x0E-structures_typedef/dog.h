9cfe5cd3c24cd8207dbb90aac7f84c63456a957f
peternmacharia <peternmacharia@outlook.com>
2022-08-03 00:46:45 +0300

    Task #0 commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a structure for dog
+ * @name: name member
+ * @age: age member
+ * @owner: owner member
+ *
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
