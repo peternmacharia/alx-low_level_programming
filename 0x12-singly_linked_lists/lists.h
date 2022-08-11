4d039ec1104799e75237027a873ce8e0ef11392f
peternmacharia <peternmacharia@outlook.com>
2022-08-11 22:00:18 +0300

    Linked lists commit #4 Task #0 and #4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef LIST_H_
+#define LIST_H_
 
 #include <stdlib.h>
 
@@ -25,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* _LIST_H_ */
+#endif /* LIST_H_ */
