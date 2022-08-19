ab83adb7f9e553f8ffadebe6623eeb32d536c2b3
peternmacharia <peternmacharia@outlook.com>
2022-08-19 04:01:28 +0300

    Tack #0 commit #7

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,24 +2,6 @@
 #define MAIN_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
