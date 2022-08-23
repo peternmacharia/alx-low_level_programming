d27a75bbbf9717d9e0c16efcc0a06431c33d5dfe
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:31:36 +0300

    File input output commit #10

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,11 +14,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
@@ -28,12 +26,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rcount = read(fd, buffer, letters);
 	if (rcount == -1)
 		return (0);
-
 	wcount = write(STDOUT_FILENO, buffer, rcount);
 	if (wcount == -1 || rcount != wcount)
 		return (0);
 	free(buffer);
-
 	close(fd);
+
 	return (wcount);
 }
