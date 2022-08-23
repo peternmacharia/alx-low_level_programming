41a45c18d1aa8875cf1f3ec8b113c9a4f950e7d7
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:28:13 +0300

    File input output commit #8

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -27,5 +28,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 #endif /* MAIN_H */
