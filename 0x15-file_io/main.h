86ed72c23217946b078cd159dd58fd3e2992cdb2
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:29:39 +0300

    File input output commit #9

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -28,4 +28,5 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+
 #endif /* MAIN_H */
