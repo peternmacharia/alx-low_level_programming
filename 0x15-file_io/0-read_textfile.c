c14157e5ac995a1600d9b534ef75bdac9669cb0a
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:07:28 +0300

    File input output commit #1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,6 @@
  * @letters: number of letters to read and print
  * Return: number of letters read and printed, or 0 if error
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
