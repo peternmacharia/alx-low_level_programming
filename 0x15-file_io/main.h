8e766d35bb143e29e7dcf176be5933e6c767349b
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:52:58 +0300

    File input output task #0 commit #8

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,14 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_N
 
-#include <stdio.h>
+#include <elf.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <elf.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
@@ -28,5 +26,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
-#endif /* MAIN_H */
+#endif
