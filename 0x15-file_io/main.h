6e71c3d850aebe4b35414c7b6d26d7349128397b
peternmacharia <peternmacharia@outlook.com>
2022-08-23 07:57:43 +0300

    File input output task #0 commit #12

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,10 +14,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-/**
- * ELF header functions
- */
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
@@ -29,4 +25,4 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
-#endif
+#endif /* MAIN_H */
