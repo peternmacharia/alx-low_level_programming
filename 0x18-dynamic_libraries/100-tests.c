6dd9d53e9183b955e856e054b5c85c36b6cc60b2
peternmacharia <peternmacharia@outlook.com>
2022-10-06 23:52:11 +0300

    Commit Task #2

---
+++ 0x18-dynamic_libraries/100-tests.c
@@ -0,0 +1,48 @@
+int add(int a, int b)
+{
+    /**
+     * add - addition function
+     * @a: first integer
+     * @b: second integer
+     *
+     * Return: the addition of the two integers
+     */
+
+    return (a + b);
+}
+
+int sub(int a, int b)
+{
+    /**
+     * sub - subtraction function
+     * @a: first integer
+     * @b: second integer
+     *
+     * Return: the subtraction of the two integers
+     */
+    return (a - b);
+}
+
+int mul(int a, int b)
+{
+    /**
+     * mul - multiplication function
+     * @a: first integer
+     * @b: second integer
+     *
+     * Return: the multiplication of the two integers
+     */
+    return (a * b);
+}
+
+int div(int a, int b)
+{
+    /**
+     * div - division function
+     * @a: first integer
+     * @b: second integer
+     *
+     * Return: the division of the two integers
+     */
+    return (a / b);
+}
